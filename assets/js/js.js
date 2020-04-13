ccfc7f8d28b508764a5ff91b966fe2a2c10b5d68
YATHARTH VARDAN <yatharthvardan@gmail.com>
2020-04-14 02:29:37 +0530

    sd

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,5 +1,9 @@
 //Common js
-
+var home_page= document.getElementById("Home_Page");
+var team_page = document.getElementById("Team_Page");
+var close_btn = document.getElementById("close_btn");
+var menu_btn = document.getElementById("menu_btn");
+var menu = document.getElementById("menu");
 
 
 // End of common js
@@ -27,3 +31,47 @@ after_fr.style.marginTop  = height_of_mock_up + 20 + "px";
 
 
 // end of Js for team page
+
+
+// js for the menu btn
+
+
+
+function open_menu()
+{
+    if(home_page.style.display!="none")
+    {
+        home_page.classList.add("is_blurred");
+    }
+    else
+    {
+        team_page.classList.add("is_blurred");    
+    }
+    menu.style.display = "block";
+    menu_btn.style.display = "none";
+}
+function close_menu()
+{
+    home_page.classList.remove("is_blurred");
+    team_page.classList.remove("is_blurred");
+    menu.style.display = "none";
+    menu_btn.style.display = "block";
+}
+
+function change_page_to_team()
+{
+    console.log("the first funtion");
+        home_page.style.display = "none";
+        team_page.style.display="block";
+        close_menu();
+}
+
+function change_page_to_home()
+{
+    console.log("the second funtion");
+    home_page.style.display="block";
+    team_page.style.display="none";
+    close_menu();
+}
+
+
